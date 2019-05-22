a7d221639e4f7cf11226fb9d92f307d47d4c36a3
layla dantas <laylatdantas@gmail.com>
2019-05-22 23:03:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,10 @@
 def my_collect(array)
   i = 0 
   while(i < array.length)
-  array.collect
-   yield array[i]
+  
+  array[i] = yield array[i]
   i = i + 1
   end 
- my_collect = array 
+  array
   
 end 
